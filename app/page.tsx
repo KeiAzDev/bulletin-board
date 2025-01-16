8099f42820fd863cf14324f6bcbe55b3c10205d1
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-16 14:19:44 -0800

    shadcn初めての利用、card作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+      <main className="grid lg:grid-cols-3 px-4 py-4 gap-4">
+      <Card>
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque in rem sunt laboriosam repellat consequuntur est totam? Dignissimos, ab nesciunt quis a eaque blanditiis nemo itaque. Totam voluptatum tempore maiores?
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Link href={'/bbs-posts/1'} className="text-gray-500">Read More</Link>
+      </CardFooter>
+    </Card>
       </main>
-      
-    </div>
+
   );
 }
