d56b7c7aaf3ea9ad56e10be8e08e90fee5cfbcb0
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-17 09:47:38 -0800

    supabaseのデータをfetchして反映

--- app/components/BBSCard.tsx
+++ app/components/BBSCard.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
+import { BBSData } from '../types/types';
 
-const BBSCard = () => {
+interface BBSCardProps {
+  bbsData: BBSData;
+}
+
+const BBSCard = ({bbsData}: BBSCardProps) => {
+
+  const {id, title, content, createdAt, username} = bbsData;
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{username}</CardDescription>
       </CardHeader>
       <CardContent>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque in rem sunt laboriosam repellat consequuntur est totam? Dignissimos, ab nesciunt quis a eaque blanditiis nemo itaque. Totam voluptatum tempore maiores?
+        {content}
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Link href={'/bbs-posts/1'} className="text-gray-500">Read More</Link>
+        <Link href={`/bbs-posts/${id}`} className="text-gray-500">Read More</Link>
       </CardFooter>
     </Card>
   )
--- app/components/BBSCardList.tsx
+++ app/components/BBSCardList.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import BBSCard from './BBSCard'
+import { BBSData } from '../types/types'
 
-const BBSCardList = () => {
+interface BBSCardListProps {
+  bbsAllData: BBSData[];
+}
+
+const BBSCardList = ({bbsAllData}: BBSCardListProps) => {
   return (
     <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
-      <BBSCard />
-      <BBSCard />
-      <BBSCard />
+      {bbsAllData.map((bbsData: BBSData) => (
+        <BBSCard key={bbsData.id} bbsData={bbsData} />
+      ))}
     </div>
   )
 }
