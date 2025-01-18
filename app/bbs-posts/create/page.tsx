d0975e3ae3f7bbaf2438abaaabaca0e8188744ea
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-17 16:13:58 -0800

    完成！！！

--- app/bbs-posts/create/page.tsx
+++ app/bbs-posts/create/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { postBBS } from '@/app/actions/postBBSAction'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
@@ -9,7 +10,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-const formSchema = z.object({
+export const formSchema = z.object({
   username: z.string().min(2, {message:'ユーザー名は２文字以上で入力してください'}),
   title: z.string().min(2, {message:'タイトルは２文字以上で入力してください'}),
   content: z.string().min(10, {message:'本文はは10文字以上で入力してください'}).max(140, {message: '本文は140文字以内で入力してください'}),
@@ -30,18 +31,19 @@ const CreateBBSPage = () => {
 
   async function onSubmit(value: z.infer<typeof formSchema>) {
     const {username, title, content} = value;
-    try {
-      await fetch('http://localhost:3000/api/post', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({username, title, content}),
-      });
-      router.push('/');
-    } catch(err) {
-      console.error(err);
-    }
+    postBBS({username, title, content});
+    // try {
+    //   await fetch('http://localhost:3000/api/post', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({username, title, content}),
+    //   });
+    //   router.push('/');
+    // } catch(err) {
+    //   console.error(err);
+    // }
   }
 
   return (
